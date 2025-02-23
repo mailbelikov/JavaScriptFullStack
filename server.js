389bc6ae3bb09a7f681c7bf182aa3dd22b89ada1
mailbelikov <mailbelikov@yandex.ru>
2025-02-23 11:39:18 +0400

    My server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-function MyServer() {
-    
-}
+const http = require("http");
+const url = require("url");
+
+const MyServer = http.createServer(function(req, resp) {
+    const pars = url.parse(req.url, true).query;
+    const name = pars['name'];
+    resp.statusCode = 200;
+    resp.setHeader('Content-Type', 'text/plain');
+    console.log(name);
+    if (name) {
+        resp.end("Hello, " + name);
+    }
+    else {
+        resp.end("Hello, Anonyme");
+    }    
+});
+
+MyServer.listen(8000);
